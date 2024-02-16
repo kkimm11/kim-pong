d442bd98cba089ccbdd3e7ec113c5b10b6e183c2
minjunkim0205 <minjunkim0205@gmail.com>
2024-02-16 23:32:00 +0900

    Update Imitation-GamePingPong

--- PingPong.py
+++ PingPong.py
@@ -5,7 +5,7 @@ import random
 # Game option
 WINDOW_HEIGHT = 360
 WINDOW_WIDTH = 640
-WINDOW_FPS = 60
+WINDOW_FPS = 144
 
 # Color
 BLACK = pygame.Color(0, 0, 0)
@@ -29,6 +29,21 @@ running = True
 time = 0
 score = 0
 
+# Ball
+ball_size = 10
+ball = [random.randrange(ball_size // 2, WINDOW_HEIGHT // 2 - ball_size // 2),
+        random.randrange(ball_size // 2, WINDOW_WIDTH - ball_size // 2)]
+ball_direction = [-2, 2]
+ball_time = 0
+ball_tick = 10
+
+# Bar
+bar = [WINDOW_HEIGHT - 20, WINDOW_WIDTH / 2]
+bar_size = 50
+bar_direction = "none"
+bar_time = 0
+bar_tick = 1
+
 # Main loop
 while running:
     # Frame Per Second / Refresh Rate
@@ -37,26 +52,63 @@ while running:
 
     # Key event
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                pass
+                bar_direction = "left"
             if event.key == pygame.K_RIGHT:
-                pass
+                bar_direction = "right"
             if event.key == pygame.K_SPACE:
                 pass
             if event.key == pygame.K_ESCAPE:
                 running = False
-
         if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                bar_direction = "none"
+            if event.key == pygame.K_RIGHT:
+                bar_direction = "none"
             if event.key == pygame.K_SPACE:
                 pass
 
-    # Ball, Bar
+    # Bar
+    if time - bar_time >= bar_tick:
+        if bar_direction == "left":
+            bar[1] += -2
+        if bar_direction == "right":
+            bar[1] += 2
+        bar_time = time
+
+    # Ball
+    if time - ball_time >= ball_tick:
+        ball[0] += ball_direction[0]
+        ball[1] += ball_direction[1]
+        ball_time = time
+
+    if ball[1] <= (0 + ball_size / 2):
+        ball[1] += 10
+        ball_direction[1] *= -1
+    if (WINDOW_WIDTH - ball_size / 2) <= ball[1]:
+        ball[1] += -10
+        ball_direction[1] *= -1
+    if ball[0] <= (0 + ball_size / 2):
+        ball[0] += 10
+        ball_direction[0] *= -1
+    if (bar[0] - ball_size / 2) <= ball[0] <= (bar[0] + 5 - ball_size / 2) and bar[1] <= ball[1] <= bar[1] + bar_size:
+        ball[0] += -10
+        ball_direction[0] *= -1
+        score += 500
+        if ball_tick > 0:
+            ball_tick += -1
 
     # Game over
+    if (WINDOW_HEIGHT - ball_size / 2) <= ball[0]:
+        running = False
 
     # Refresh game screen
     window.fill(BLACK)
+    pygame.draw.circle(window, WHITE, [ball[1], ball[0]], ball_size / 2)
+    pygame.draw.rect(window, GREEN, [bar[1], bar[0], bar_size, 5])
     window.blit(pygame.font.SysFont(None, 25).render(str(score), True, WHITE), (10, 10))
     pygame.display.update()
 
